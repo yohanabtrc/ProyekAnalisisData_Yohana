238325c5d1171ca979f5ca60f0b814daf1815357
yohanabtrc <118099626+yohanabtrc@users.noreply.github.com>
2024-03-04 00:39:57 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,8 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from babel.numbers import format_currency
-
 
 st.set_page_config(page_title="Dashboard E-commerce by Yohana", page_icon=":bar_chart", layout="wide")
 st.title(" :bar_chart: Dashboard E-commerce by Yohana :bar_chart:")
